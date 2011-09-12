3b500f40b021d685d991dc94b97176e920cc249e
Michael Frericks <Michael.Frericks@f-i.de>
2011-09-12 15:29:26 +0200

    doc core.clj

--- src/knot/core.clj
+++ src/knot/core.clj
@@ -13,7 +13,9 @@ Returns a sequence of the symbol names of the declared namespaces."
 		      (filter map? %)))
        (map second)))
 
-(defn ns-intern-with-meta-key [ns meta-key]
+(defn- ns-intern-with-meta-key
+  "Returns the meta-data of vars defined in namespace ns only if the meta-data contains the key meta-key"
+  [ns meta-key]
   (->> (ns-interns ns)
        (vals)
        (map meta)
@@ -21,40 +23,36 @@ Returns a sequence of the symbol names of the declared namespaces."
        )
   )
 
-
-(defn extension-points [ns]
+(defn extension-points
+  "Returns the meta-data of vars defined in namespace ns only if the meta-data contains the key :extension-point-id"
+  [ns]
   (ns-intern-with-meta-key ns :extension-point-id))
 
-(defn extensions [ns]
+(defn extensions
+  "Returns the meta-data of vars defined in namespace ns only if the meta-data contains the key :extension-point-ref"
+  [ns]
   (ns-intern-with-meta-key ns :extension-point-ref))
 
 (defn build-registry
-  ""
+  "Builds the registry for the namespaces in seq knot-modules. The registry is map. It maps the extension-point-id to a seq of the meta-data of the extensions to that extension-point-id. Calls require for the namespaces in knot-modules in order to have access to the vars. Returns the registry."
   [knot-modules]
   (doseq [ns knot-modules]
     (require ns))
-  (let [ext-points (flatten (map extension-points
-				 knot-modules))
-	ext        (flatten (map extensions
-				 knot-modules))
-	filter-extensions (fn [extension ext-point-id]
-			    (= ext-point-id
-			       (get extension :extension-point-ref)))]
-    (reduce (fn [a ep]
-	      (let [ext-point-id (get ep :extension-point-id)]
-		(assoc a ext-point-id
-		       (filter #(filter-extensions % ext-point-id) ext)
-		       )
-		)
-	      )
+  (let [ext-point-ids     (->> knot-modules
+			       (map extension-points)
+			       (flatten)
+			       (map :extension-point-id))
+	extensions        (flatten (map extensions knot-modules))
+	filter-extensions (fn [extension ext-point-id] (= ext-point-id (get extension :extension-point-ref)))]
+    (reduce (fn [a ext-point-id] (assoc a ext-point-id (filter #(filter-extensions % ext-point-id) extensions)))	      
 	    {}
-	    ext-points)))
-    
+	    ext-point-ids)))
 
-(def *registry* (build-registry (knot-module-namespaces)))
-  
+(def- *registry* (build-registry (knot-module-namespaces)))
 
-(defn configuration-elements-for [extension-point-id]
+(defn configuration-elements-for
+  "Returns all extensions contributed to the extension-point-id"
+  [extension-point-id]
   (->> (get *registry* extension-point-id)
        (map #(ns-resolve (get % :ns) (get % :name)))
        (map deref)))
